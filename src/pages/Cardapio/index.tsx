2d6848f1a352e82baabf5eb9f103f248cea40167
Bruna <brunadias_30@hotmail.com>
2023-03-06 09:42:54 -0300

    importando logo como componente

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,10 @@
 import style from './Cardapio.module.scss'
-import logo from '../../assets/logo.svg'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
 export default function Cardapio() {
     return (
         <main>
             <nav className={style.menu}>
-                <img src={logo} alt='logo do aluroni' />
+                <Logo />
             </nav>
         </main>
     )
