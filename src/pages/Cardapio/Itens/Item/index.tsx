98c3361d4d15cb8202dcf4a0e0eb8fbb49d10ea4
Bruna <brunadias_30@hotmail.com>
2023-03-08 19:31:51 -0300

    alocando imagens dinamicamente e colocando em producao

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './Item.module.scss'
 import cardapio from '../itens.json'
+import classNames from 'classnames'
 
 type Props = typeof cardapio[0]
 
@@ -9,7 +10,7 @@ export default function Item(props: Props) {
     return (
         <div className={style.item}>
             <div>
-                <img src='' alt={title} />
+                <img src={photo} alt={title} />
             </div>
             <div className={style.item__descricao}>
                 <div className={style.item__titulo}>
@@ -17,7 +18,13 @@ export default function Item(props: Props) {
                     <p> {description} </p>
                 </div>
                 <div className={style.item__tags}>
-                    <div className={style.item__tipo}>{category.label}</div>
+                    <div 
+                        className={classNames({
+                            [style.item__tipo]: true,
+                            [style[`item__tipo__${category.label.toLowerCase()}`]]: true,
+                        })}
+                    >
+                        {category.label}</div>
                     <div className={style.item__porcao}>{size}g</div>
                     <div className={style.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : "s"}</div>
                     <div className={style.item__valor}>R$ {price}</div>
